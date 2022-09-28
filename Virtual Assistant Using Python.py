c385847f2c8af522b52f30e6c7621133796b5635
Proxlight <69844328+Proxlight@users.noreply.github.com>
2022-09-28 16:01:05 +0530

    Update Virtual Assistant Using Python.py

--- Virtual Assistant Using Python.py
+++ Virtual Assistant Using Python.py
@@ -143,7 +143,7 @@ def Take_query():
 
         # this will exit and terminate the program
         elif "bye" in query:
-            speak("Bye. Check Out GFG for more exciting things")
+            speak("Bye. Check Out Proxlight for more exciting things")
             exit()
 
         elif "from wikipedia" in query:
